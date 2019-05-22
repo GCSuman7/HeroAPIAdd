fecbd9e70d45d737205d8c00cf24fa8117f588dd
Softwarica College Lab <softwarica@softwarica.com>
2019-05-22 11:50:37 +0545

    Initial commit

---
+++ app/src/main/java/API/HeroesAPI.java
@@ -0,0 +1,21 @@
+package API;
+
+import model.ImageResponse;
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface HeroesAPI {
+    @FormUrlEncoded
+    @POST("heroes")
+    Call<Void> addHero(@Field("name") String name, @Field("desc") String desc);
+
+    @Multipart
+    @POST("upload")
+    Call<ImageResponse> uploadImage(@Part MultipartBody.Part img);
+
+}
---
+++ app/src/main/java/model/ImageResponse.java
@@ -0,0 +1,15 @@
+package model;
+
+import android.os.StrictMode;
+
+import java.io.File;
+
+public class ImageResponse {
+    private String filename;
+
+    public String getFilename(){
+        return filename;
+    }
+
+
+}
